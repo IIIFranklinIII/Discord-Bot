130a0d028fd53cd2a95f9560862f02b83891982c
Franklin <100046782+IIIFranklinIII@users.noreply.github.com>
2022-10-15 17:16:28 +0300

    Update GuildListener.java

--- GuildListener.java
+++ GuildListener.java
@@ -26,7 +26,19 @@ public class GuildListener extends ListenerAdapter {
         if(content.equals("!sayHi")) {
             channel.sendMessage("Hello everyone!").queue();
         }
-       
+        
+        else if(content.equals("!help")) {
+            channel.sendMessage("!!Here you can read all of my text listeners!! \n \n" +
+                                     "1) !sayHi - make the bot say *Hello everyone* \n" +
+                                     "2) !help - display command list \n" +
+                                     "3) /welcome - get welcomed by the bot" +
+                                     "4) /roles - display all roles on the server" +
+                                     "5) /scgv - secret cat-girl voice" +
+                                     "6) /say - make the bot say a message" +
+                                     "7) /emote - express your emotions through text" +
+                                     "8) /giverole - give a user a role" +
+                                     "9) /getacat - bot sends you a cat picture").queue();
+        }
         System.out.println("Message received from " +
                 event.getAuthor().getName() +
                 ": " +
